ef7ff6b330ff087d130d80eb38dd00945f172af1
Raushan Raj <raushanraj7156@gmail.com>
2023-09-18 14:20:14 +0530

    const added

--- Javascript/8_Js Let.js
+++ Javascript/8_Js Let.js
@@ -6,10 +6,10 @@
 
  Variables defined with let have Block Scope */
 
- /*
- Variables defined with let can not be redeclared.
+/*
+Variables defined with let can not be redeclared.
 
- let x = "John Doe";
+let x = "John Doe";
 
 let x = 0;
 
@@ -28,12 +28,12 @@ console.log(x);
 
 // Variables declared inside a { } block cannot be accessed from outside the block:
 {
-    let m = 2;
-  }
-  // x can NOT be used here
-  //console.log(m);
+  let m = 2;
+}
+// x can NOT be used here
+//console.log(m);
 
-  {
+{
   const n = 2;
 }
 // n can NOT be used here
@@ -44,7 +44,7 @@ console.log(x);
 //Variables declared inside a { } block can be accessed from outside the block.
 
 {
-    var a=6;
+  var a = 6;
 }
 // a can be used here
 console.log(a);
@@ -58,8 +58,8 @@ var x = 10;
 // Here x is 10
 
 {
-var x = 2;
-// Here x is 2
+  var x = 2;
+  // Here x is 2
 }
 
 // Here x is 2
@@ -130,6 +130,11 @@ console.log(carName);
 // Meaning: Using a let variable before it is declared will result in a ReferenceError:
 
 
-countryName = "INDIA";
-let countryName = "INDIA";
+// countryName = "INDIA";
+// let countryName = "INDIA";
 
+// variables declare with Let can be reassign
+
+let q = 12;
+q = 14;
+console.log(q);
