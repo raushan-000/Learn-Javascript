ca65ec5540fbceb267200aa1fb9428a14d6b7cb8
Raushan Raj <raushanraj7156@gmail.com>
2023-11-23 19:35:51 +0530

    execution context

--- JavaScriptwithChai/basics/04_IIFE..js
+++ JavaScriptwithChai/basics/04_IIFE..js
@@ -5,5 +5,18 @@
     console.log('db connected');
 })();
 
+// if we don't use ; before IIFE, it will give error
+(function chai(){
+    console.log('db connected1');
+})();
 
+function chai(){
+    // named iife
+    console.log('db connected3');
+}
+chai();
 
+// umnamed iife with parameter
+( (name) => {
+    console.log(`db connected ${name}`);
+})('raushan')
---
+++ JavaScriptwithChai/basics/05_ExecutionContext.js
@@ -0,0 +1,9 @@
+/**
+ * Javascript execution context
+ * {} --> Global execution context --> This
+ * Function Execution Context
+ * Eval execution context
+ * 
+ * 1. Memory Creation Phase
+ * 2. Execution Phase
+ */
