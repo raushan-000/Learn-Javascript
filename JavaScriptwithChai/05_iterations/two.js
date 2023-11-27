5bc7326d274ba9f69e519aa5d0262b21ab89527c
Raushan Raj <raushanraj7156@gmail.com>
2023-11-27 22:14:13 +0530

    loop

---
+++ JavaScriptwithChai/05_iterations/two.js
@@ -0,0 +1,21 @@
+// while
+let index=0
+while (index<=10) {
+    console.log(`value of index is ${index}`);
+    index=index+2
+}
+
+let myArray=["car","bus","cycle"]
+let arr=0
+while (arr<myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+    arr = arr+1;
+}
+
+
+// do while
+let score =1
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score<=10);
